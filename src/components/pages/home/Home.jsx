a2cab743338717bf1935d70a3318cd34b6827e3c
Hoang Trung Duc <60508265+htrDuc@users.noreply.github.com>
2021-12-23 10:35:17 +0700

    first commit

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from "../../header/Header";
+import Posts from "../../posts/Posts";
+import SideBar from "../../sidebar/SideBar";
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <div className="home">
+                <Posts />
+                <SideBar />
+            </div>
+        </>
+    );
+};
+
+export default Home;
