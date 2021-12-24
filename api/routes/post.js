a4cca51c8e26ce073aa94590e554fb1bf7f750d8
Hoang Trung Duc <60508265+htrDuc@users.noreply.github.com>
2021-12-24 15:36:37 +0700

    request query

--- api/routes/post.js
+++ api/routes/post.js
@@ -64,4 +64,27 @@ router.get("/:id", async (req, res) => {
     }
 })
 
+//GET ALL POST
+router.get("/", async (req, res) => {
+    const username = req.query.user; //get theo username
+    const catName = req.query.catName; //get theo name category
+    try {
+        let posts;
+        if (username) {
+            posts = await Post.find({username});
+        } else if (catName) {
+            posts = await Post.find({
+                categories: {
+                    $in: [catName]
+                }
+            })
+        } else {
+            posts = await Post.find();
+        }
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 module.exports = router;
