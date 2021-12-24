e3adfe7728390ecbdd18939f3e51f09f6e968e62
Hoang Trung Duc <60508265+htrDuc@users.noreply.github.com>
2021-12-24 12:01:54 +0700

    login and register api

--- api/index.js
+++ api/index.js
@@ -1,17 +1,17 @@
 const express = require("express")
 const app = express()
-const dotenv =  require("dotenv");
+const dotenv = require("dotenv");
 const mongoose = require('mongoose');
-
+const authRoute = require("./routes/auth")
 dotenv.config();
+app.use(express.json()); //de nhan dc file json
 
 mongoose.connect(process.env.MONGO_URL)
-.then(console.log("Connected to Mongodb"))
-.catch((err) => console.log(err))
+    .then(console.log("Connected to Mongodb"))
+    .catch((err) => console.log(err))
 ;
-app.get("/" ,(req, res) => {
-    console.log("hello")
-})
+
+app.use("/api/auth", authRoute)
 
 app.listen(5000, () => {
     console.log("Backend is running")
